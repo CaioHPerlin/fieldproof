38151e9b3d62ef1d84d45a87f142fb86e0c7cae6
caiohperlin <caiohperlin@gmail.com>
2026-06-24 12:57:12 -0400

    chore: define theme constants

---
+++ theme/colors.ts
@@ -0,0 +1,17 @@
+export const colors = {
+  primary: "#6200ea",
+  success: "#28a745",
+  danger: "#df3226",
+  link: "#6200eaae",
+  overlay: "rgba(0,0,0,0.55)",
+  overlayDark: "rgba(0,0,0,0.8)",
+  surface: "#fff",
+  background: "#f9f9f9",
+  border: "#ccc",
+  text: {
+    primary: "#333",
+    secondary: "#666",
+    muted: "#888",
+    white: "#fff",
+  },
+};
